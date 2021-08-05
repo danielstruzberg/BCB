f6703cbb8e911fc0e70fa068763a384e6c4259ff
Daniel Chomentowski Struzberg <dstruzberg@bog.local>
2021-08-05 20:44:44 -0300

    Atualizando

--- etl.R
+++ etl.R
@@ -2,15 +2,26 @@
 
 library(tidyverse)
 library(dplyr)
+library(here)
 
 
 ano<-tibble(ano=c(2017:2021))
 periodo<-tibble(periodo=c(1:4))
-periodos_ano <- crossing(ano,periodo)
+periodos_ano <- crossing(ano,periodo) %>% 
+    mutate(arquivo_destino=str_glue("Dados/{ano}-{periodo}") %>% 
+               here()) 
 
 
 caminhos<-str_glue('https://www3.bcb.gov.br/rdrweb/rest/ext/ranking/arquivo?ano={periodos_ano$ano}&periodicidade=TRIMESTRAL&periodo={periodos_ano$periodo}&tipo=Bancos%20e%20financeiras')
 
 
-purrr::walk(caminhos,.f=download.file)
+purrr::walk2(.x=caminhos,.y=periodos_ano$arquivo_destino,.f=~download.file(url=.x,destfile = .y))
 
+Arquivos <- list.files(here('Dados'),full.names = TRUE)
+
+Dados_Cliente_Banco <- read_csv2(Arquivos[2]) %>% 
+    
+    
+
+
+reader
